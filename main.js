18e98944062e5eca4c726e426438e5751addecd2
Leulmengistu <leulmengistu64@gmail.com>
2022-10-06 10:38:52 +0300

    First commit to this repo

---
+++ main.js
@@ -0,0 +1,11 @@
+const time = 5;
+const second = time;
+let lim;
+// let currentRoom = currentRoomA;
+console.log("De Morgan’s law: \u2310(P\u22c0Q) \u21D4 (\u2310P) \u22c1 (\u2310Q)");
+let currentTemp = 19.5;
+// 00b0 is the Unicode code point for the "degree" symbol
+const message = "The current temperature is " + currentTemp + "\u00b0C";
+var $ = 5;
+const x = $;
+console.log($);
